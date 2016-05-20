2c0b60a51607a4403f58a2017d405d80b687a8b3
Greg Zimmerman <thezim@gmail.com>
2016-05-19 21:40:25 -0400

    Convert to fucntion.

--- posh-coldfusion.ps1
+++ posh-coldfusion.ps1
@@ -1,23 +1,38 @@
-﻿$seed = "4E1C4C42AC14152C"
-$enc = "60OO4KORzVHnJ5TGgye0YvWqKmZ1hH5NYjFaEYSrrq9DHu4i7/3+8WWcDCiqORGf"
+﻿function Decrypt-CipherText {
+    param(
+        [string]$Base64,
+        [string]$Seed,
+        [int]$BlockSize=128,
+        [int]$KeySize=128
+    )
 
-$keybytes = [System.Text.Encoding]::UTF8.GetBytes($seed)
-$encbytes =  [Convert]::FromBase64String($enc)
+    # convert to bytes
+    $keybytes = [System.Text.Encoding]::UTF8.GetBytes($Seed)
+    $encbytes =  [Convert]::FromBase64String($Base64)
 
-$algorithm = New-Object System.Security.Cryptography.RijndaelManaged
-$algorithm.Mode = [System.Security.Cryptography.CipherMode]::CBC
-$algorithm.Padding = [System.Security.Cryptography.PaddingMode]::PKCS7
-$algorithm.BlockSize = 128
-$algorithm.KeySize = 128
-$algorithm.Key = $keybytes
-$iv = New-Object byte[] 16
-[Array]::Copy($encbytes, $iv, 16)
-$enclen = $encbytes.Length - 16
-$encnew = New-Object byte[] $enclen
-[Array]::Copy($encbytes, 16, $encnew, 0, $enclen)
-$algorithm.IV = $iv
-$decryptor = $algorithm.CreateDecryptor()
-$ms = New-Object System.IO.MemoryStream -ArgumentList @(,$encnew)
-$cs = New-Object System.Security.Cryptography.CryptoStream($ms, $decryptor, "Read")
-$sr = New-Object System.IO.StreamReader($cs)
-$sr.ReadToEnd()
+    $size = $BlockSize / 8
+
+    # get IV
+    $iv = New-Object byte[] $size
+    [Array]::Copy($encbytes, $iv, $size)
+
+    # get cipher text
+    $enclen = $encbytes.Length - $size
+    $enc = New-Object byte[] $enclen
+    [Array]::Copy($encbytes, $size, $enc, 0, $enclen)
+
+    # decrypt
+    $aes = New-Object System.Security.Cryptography.RijndaelManaged
+    $aes.Mode = [System.Security.Cryptography.CipherMode]::CBC
+    $aes.Padding = [System.Security.Cryptography.PaddingMode]::PKCS7
+    $aes.BlockSize = $BlockSize
+    $aes.KeySize = $KeySize
+    $aes.Key = $keybytes
+    $aes.IV = $iv
+    $decryptor = $aes.CreateDecryptor()
+    $ms = New-Object System.IO.MemoryStream -ArgumentList @(,$enc)
+    $cs = New-Object System.Security.Cryptography.CryptoStream($ms, $decryptor, "Read")
+    $sr = New-Object System.IO.StreamReader($cs)
+    $sr.ReadToEnd()
+}
+Decrypt-CipherText -Seed $seed -Base64 $ciphertext
