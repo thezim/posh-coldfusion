644046f4a00c48cf2beeec4c3c9bbd9c6ad68686
thezim <thezim@gmail.com>
2016-05-21 10:08:44 -0400

    Add encrypt function.

--- posh-coldfusion.ps1
+++ posh-coldfusion.ps1
@@ -1,4 +1,38 @@
-﻿function Decrypt-CipherText {
+﻿function Encrypt-Text {
+    param(
+        [string]$Text,
+        [string]$Seed,
+        [int]$BlockSize=128,
+        [int]$KeySize=128
+    )
+    
+    # get key btes
+    $keybytes = [System.Text.Encoding]::UTF8.GetBytes($Seed)
+    
+    # encrypt data
+    $aes = New-Object System.Security.Cryptography.RijndaelManaged
+    $aes.Mode = [System.Security.Cryptography.CipherMode]::CBC
+    $aes.Padding = [System.Security.Cryptography.PaddingMode]::PKCS7
+    $aes.BlockSize = $BlockSize
+    $aes.KeySize = $KeySize
+    $aes.Key = $keybytes
+    $aes.GenerateIV()
+    $encryptor = $aes.CreateEncryptor()
+    $ms = New-Object System.IO.MemoryStream
+    $cs = New-Object System.Security.Cryptography.CryptoStream($ms, $encryptor, "Write")
+    $sw = New-Object System.IO.StreamWriter($cs)
+    $sw.Write($Text)
+    $sw.Close()
+    $enc = $ms.ToArray()
+
+    # generate base64 IV + cipher text
+    $bytes = New-Object byte[] ($aes.IV.Count + $enc.Count)
+    [Array]::Copy($aes.IV, 0, $bytes, 0, $aes.IV.Count)
+    [Array]::Copy($enc, 0, $bytes, $aes.IV.Count, $enc.Count)
+    [Convert]::ToBase64String($bytes)
+}
+
+function Decrypt-CipherText {
     param(
         [string]$Base64,
         [string]$Seed,
@@ -35,4 +69,3 @@
     $sr = New-Object System.IO.StreamReader($cs)
     $sr.ReadToEnd()
 }
-Decrypt-CipherText -Seed $seed -Base64 $ciphertext
