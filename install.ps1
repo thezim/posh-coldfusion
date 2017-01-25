0dac6765c6e2ae62f3beca4b073a12bb67b994a1
Greg Zimmerman <thezim@gmail.com>
2017-01-25 16:34:16 -0500

    Add installer.

---
+++ install.ps1
@@ -0,0 +1,7 @@
+$files = @("posh-coldfusion.psm1", "README.md")
+$delimiter = [System.IO.Path]::PathSeparator
+$modpath = "{0}\posh-stig" -f ($env:PSModulePath -split $delimiter | Select-Object -First 1)
+if((Test-Path -Path $modpath) -eq $false){
+    New-Item -ItemType Directory -Path $modpath | Out-Null
+}
+Copy-Item -Force -Path $files -Destination $modpath
